32aae41376418877dd3555fcc003817f4df82710
fabiiianac15 <fjacostaa@ufpso.edu.co>
2023-05-10 17:13:30 -0500

    learning js

---
+++ js/clase.js
@@ -0,0 +1,40 @@
+// VARIABLES//
+const nameuser = "Fabian Acosta";
+var x= "Valor";
+let y= "Valor";
+
+let a = 20;
+let b = 5.4;
+let String = " Mi nombre es Fabian Acosta";
+let number = "045435243";
+let boolean_uno = false;
+let boolean_dos = true;
+let array = [1,2,3];
+let array_string= ["lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+
+// OPERADORES ARITMETICOS//
+let suma = 1+5+a;
+let resta = 1-5-a;
+let multi = 1*5*a;
+let divi = 1/5/a;
+let module = 10%2;
+let elev = 5^2;
+console.log("- "+suma+ "\n-"+ resta+ "\n-" + multi+ "\n-"+ divi+ "\n-" + module+ "\n-"+ elev+"\n-")
+
+//OPERADORES DE COMPARACION = / == / === / <> / >= / <= / < / > //
+
+//OPERADORES LOGICOS AND && / OR || /
+
+if(10>5){
+    for(let i=0; i<array_string.length;i++){
+        console.log(array_string[i]);
+    }
+}
+
+let j = 5;
+let k = 10;
+while(k>=j){
+    console.log("ENTRA");
+    j++;
+    break;
+}
